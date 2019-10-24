f429d8501c084091e67e61cf17e9cd90f1f425e4
Benjamin Rucker <21.benjamin.rucker@ksd.org>
2019-10-24 13:21:15 -0700

    c#

---
+++ Assets/c#/playerhealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class playerhealth : MonoBehaviour
+{
+    public int Health = 10;
+    public int lives = 10;
+    public Text healthText;
+    public Slider healthSlider;
+    private void Start()
+    {
+        healthText.text = "Health" + Health;
+        healthSlider.maxValue = Health;
+        healthSlider.value = Health;
+        
+    }
+    void OnTriggerEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Health--;
+            healthText.text = "Health" + Health;
+            healthSlider.value = Health;
+           if(Health < 1)
+            {
+                if (Health < 1)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                    PlayerPrefs.SetInt("lives", lives - 1);
+                }
+                else
+                {
+
+                }
+            }
+           
+        }
+    }
+}   
+
+    
---
+++ Assets/c#/topdownmovmeant.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class topdownmovmeant : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePostion = Input.mousePosition;
+        mousePostion = Camera.main.ScreenToWorldPoint(mousePostion);
+        mousePostion.z = transform.position.z;
+        transform.up = mousePostion - transform.position;
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 moveDir = y * transform.up + x * transform.right;
+        moveDir.Normalize();
+        GetComponent<Rigidbody2D>().velocity = moveDir * moveSpeed;
+    }
+}
