b83cb5adf4f3c08a0d65628c4b9c743b36c08626
Benjamin Rucker <21.benjamin.rucker@ksd.org>
2019-10-31 11:57:10 -0700

    making a timer

---
+++ Assets/c#/timer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timer : MonoBehaviour
+{
+    float timeleft = 60.0f;
+
+    private void Update()
+    {
+        timeleft -= Time.deltaTime;
+        if ( timeleft < 0)
+        {
+
+        }
+    }
+}
