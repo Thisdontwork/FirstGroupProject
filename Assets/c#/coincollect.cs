7084259f2b78cd9cce6ed3ac6333c9b0e3748bb8
Benjamin Rucker <21.benjamin.rucker@ksd.org>
2019-11-04 12:16:43 -0800

    gonna work on how the bullets work and the portal spawing

---
+++ Assets/c#/coincollect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class coincollect : MonoBehaviour
+{
+    public int coinCount = 0;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "coin")
+        {
+            coinCount++;
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "badcoin")
+        {
+            coinCount--;
+            Destroy(collision.gameObject);
+        }
+       
+    }
+}
